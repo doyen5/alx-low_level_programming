8dbd2053a0bfab183a37d4a2a0d7ce79f6543950
Prince Désiré <princedesirek@gmail.com>
2022-10-14 18:04:59 -0700

    Task 9 combinaison

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 48;
+	while (i < 58)
+	{
+		putchar(i);
+		if (i !=  57)
+		{
+			putchar(44);
+			putchar(32);
+			i++;
+		}
+		else
+		{
+			i++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
