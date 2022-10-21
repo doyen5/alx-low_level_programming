5b5532b2ee0b67cfa6993fcafa2b1635b8be45ae
Prince Désiré <princedesirek@gmail.com>
2022-10-21 15:28:13 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_line - entry point
+ * Description: Prints lines
+ * @n: number of lines
+ * Return: void
+ */
+void print_line(int n)
+{
+	int len;
+
+	if (n > 0)
+	{
+		for (len = 0; len < n; len++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
